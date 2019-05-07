d4127dccfb0d913760a32490806d386e7507d8b1
SpyderScrypt <spiderscrypt@gmail.com>
2019-05-07 17:48:36 +0530

    First 5

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Page1 from "./Page1/Page1";
 import Page2 from "./Page2/Page2";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Page 1</Link>
-              </li>
-              <li>
-                <Link to="/p2/">Page 2</Link>
-              </li>
-            </ul>
-          </nav>
+    <h1>asdasdasd</h1>
+    // <div className="App">
+    //   <Router>
+    //     <div>
+    //       <nav>
+    //         <ul>
+    //           <li>
+    //             <Link to="/">Page 1</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="/p2/">Page 2</Link>
+    //           </li>
+    //         </ul>
+    //       </nav>
 
-          <Route path="/" exact component={Page1} />
-          <Route path="/p2" exact component={Page2} />
-        </div>
-      </Router>
-    </div>
+    //       <Route path="/" exact component={Page1} />
+    //       <Route path="/p2" exact component={Page2} />
+    //     </div>
+    //   </Router>
+    // </div>
   );
 }
 
