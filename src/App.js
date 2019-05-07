62035e330bbab2c068095c068169504ccf5b8fbf
SpyderScrypt <spiderscrypt@gmail.com>
2019-05-07 17:26:50 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Page1 from "./Page1/Page1";
 import Page2 from "./Page2/Page2";
 
@@ -17,13 +17,11 @@ function App() {
               <li>
                 <Link to="/p2/">Page 2</Link>
               </li>
-
             </ul>
           </nav>
 
           <Route path="/" exact component={Page1} />
           <Route path="/p2" exact component={Page2} />
-
         </div>
       </Router>
     </div>
